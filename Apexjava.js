fea3d3f6fd28aa7afe13f41a947eaecc6c66d2c4
The-Ragnaross <97483803+The-Ragnaross@users.noreply.github.com>
2022-03-26 22:07:02 -0600

    Update Apexjava.js

--- Apexjava.js
+++ Apexjava.js
@@ -16,15 +16,6 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-<style>
-.panel {
-  padding: 0 18px;
-  background-color: white;
-  max-height: 0;
-  overflow: hidden;
-  transition: max-height 0.2s ease-out;
-}
-</style>
 
 <script>
 var acc = document.getElementsByClassName("accordion");
